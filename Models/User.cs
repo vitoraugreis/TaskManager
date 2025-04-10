1306474a998b8cc51cbe476c4140048583fe507d
Vitor Augusto <vitoraugreis@gmail.com>
2025-04-10 11:37:28 -0300

    Alteração do User: Agora o usuário terá um username único

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManager.Models
 {
     public class User
     {
         public int Id { get; private set; }
-        public string Name { get; set;}
 
-        public User(string name)
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        public ICollection<UserTask> Tasks { get; set; } = new List<UserTask>();
+
+        public User() { }
+        public User(string username)
         {
-            Name = name;
+            Username = username;
         }
     }
 }
